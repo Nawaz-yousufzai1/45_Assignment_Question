47bb3e44c2d4e68c4f72da371b44e53b999d1ad4
Nawaz-yousufzai1 <160153195+Nawaz-yousufzai1@users.noreply.github.com>
2024-04-07 03:28:59 +0500

    Add files via upload

---
+++ exercise#23.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+23. Conditional Tests: Write a series of conditional tests.
+ Print a statement describing each test and your prediction for the results of each test.
+ Your code should look something like this:
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+*/
+let Name = 'Qasim';
+console.log("Is name equal to 'Qasim'? I predict true");
+console.log(Name === 'Qasim');
+let x = 5;
+let y = 10;
+console.log("Is x plus y equal to 15? predict true ");
+console.log(x + y === 15);
+let age = 30;
+console.log("Is age greater than 18? I predict true. ");
+console.log(age > 18);
+let isAdmin = true;
+console.log("Is isAdmin a boolean type? I predict true .");
+console.log(typeof isAdmin === 'boolean');
+let fruits = ['apple', 'banana', 'orange'];
+console.log("Does 3 fruits names are in Array ? predict true. ");
+console.log(fruits.length);
+let city = 'Karachi';
+console.log("Is city not equal to 'London'? I predict true.");
+console.log(city !== 'London');
+let isEven = 10 % 2 === 0;
+console.log("Is 10 even? ");
+console.log(isEven);
+let isLoggedIn = false;
+console.log("Is isLoggedIn not true?");
+console.log(!isLoggedIn);
+let temperature = 25;
+console.log("Is temperature less than or equal to 0? ");
+console.log(temperature <= 0);
+let hasAccess = false;
+console.log("Does the user have access? ");
+console.log(hasAccess);
